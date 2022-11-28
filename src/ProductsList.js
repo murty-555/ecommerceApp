0487f7c7bc5ee47cd51f18f4b43671a857e1958f
hpalivela <hpalivela@msystechnologies.com>
2022-11-28 16:36:47 +0530

    added

---
+++ src/ProductsList.js
@@ -0,0 +1,82 @@
+import React from 'react'
+const App = () => {
+  return (
+    <center>
+    <div>
+      <div className='container'>
+        <div className='row px-xl-5'>
+          <div className='container col-lg-4 col-md-6 col-sm-12 pb-1 '>
+          <div style={{ backgroundColor: "white", border: "1px solid whitesmoke ", width: 'auto', height: "auto", margin: "20px" }}>
+            <p style={{ textAlign: "right", paddingRight: "10px",color:"grey" }}> 15 Products</p>
+            <div className='inner'>
+              <img alt="" src={'/images/cat-1.JPG'} className='size ' />
+            </div>
+            <h3 className='color'>
+              Men's Dresses
+            </h3>
+          </div>
+          </div>
+          <div className='container col-lg-4 col-md-6 col-sm-12 pb-1 '>
+          <div  style={{ backgroundColor: "white", border: "1px solid whitesmoke ", width: 'auto', height: "auto", margin: "20px" }} >
+            <p style={{ textAlign: "right", paddingRight: "10px", color:"grey" }}> 15 Products</p>
+            <div className='inner'>
+            <img alt="" src={'/images/cat-2.JPG'} className='size' />
+            </div>
+            <h3 className='color'>
+              Women's Dresses
+            </h3>
+          </div>
+          </div>
+          <div className='container col-lg-4 col-md-6 col-sm-12 pb-1 '>
+          <div style={{ backgroundColor: "white", border: "1px solid whitesmoke ", width: 'auto', height: "auto", margin: "20px" }} >
+            <p style={{ textAlign: "right", paddingRight: "10px",color:"grey" }}> 15 Products</p>
+            <div className='inner'>
+            <img alt="" src={'/images/cat-3.JPG'} className='size' />
+            </div>
+            <h3 className='color'>
+              Baby's Dresses
+            </h3>
+          </div>
+          </div>
+          <div className='container col-lg-4 col-md-6 col-sm-12 pb-1 '>
+          <div style={{ backgroundColor: "white", border: "1px solid whitesmoke ", width: 'auto', height: "auto", margin: "20px" }} >
+            <p style={{ textAlign: "right", paddingRight: "10px",color:"grey" }}> 15 Products</p>
+            <div className='inner'>
+            <img alt="" src={'/images/cat-4.JPG'} className='size' />
+            </div>
+            <h3 className='color'>
+              Accerssories
+            </h3>
+          </div>
+          </div >
+          <div className='container col-lg-4 col-md-6 col-sm-12 pb-1 '>
+          <div style={{ backgroundColor: "white", border: "1px solid whitesmoke ", width: 'auto', height: "auto", margin: "20px" }} >
+            <p style={{ textAlign: "right", paddingRight: "10px",color:"grey" }}> 15 Products</p>
+            <div  className='inner'>
+            <img alt="" src={'/images/cat-5.JPG'} className='size' />
+            </div>
+            <h3 className='color'>
+              Bags
+            </h3>
+          </div>
+          </div>
+          <div className='container col-lg-4 col-md-6 col-sm-12 pb-1 '>
+          <div style={{ backgroundColor: "white", border: "1px solid whitesmoke ", width: 'auto', height: "auto", margin: "20px" }} >
+            <p style={{ textAlign: "right", paddingRight: "10px",color:"grey" }}> 15 Products</p>
+            <div className='inner'>
+            <img alt="" src={'/images/cat-6.JPG'} className='size' />
+            </div>
+            <h3 className='color'>
+              Shoes
+            </h3>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </center>
+
+  )
+}
+
+export default App
