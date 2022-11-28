679967b668127b7d59be61e8f865e2665c3daa90
snvdmahesh <nsiriki@msystechnologies.com>
2022-11-28 15:52:02 +0530

    thirdportionadded

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import Header from './Components/Header';
 import Carosal from './Components/Carosal';
+import Trendy from './Trendy'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Carosal/>
+      <Trendy/>
      </div>
   );
 }
