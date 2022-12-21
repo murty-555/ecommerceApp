442c42f289a342e208de004ed5ec72c0c86a7a26
Bhargavi-yarlagadda-22 <bhargaviyarlagadda98@gmail.com>
2022-12-21 17:10:29 +0530

    changes added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,18 +5,18 @@ const Header=()=>{
     
     return(
         <>
-        <div class="container-fluid" >
+        <div class="container-fluid header-container" >
         <div class="row  py-2 px-xl-5" style={{backgroundColor:"#e6e6fa"}}>
             <div class="col-lg-6 d-none d-lg-block" >
                 <div class="d-inline-flex align-items-center">
-                    <a class="text-dark" href="">FAQs</a>
+                    <a class="text-dark " href="">FAQs</a>
                     <span class="text-muted px-2">|</span>
                     <a class="text-dark" href="">Help</a>
                     <span class="text-muted px-2">|</span>
                     <a class="text-dark" href="">Support</a>
                 </div>
             </div>
-            <div class="col-lg-6 text-center text-lg-right">
+            <div class="col-lg-6  text-lg-right">
                 <div class="d-inline-flex align-items-center">
                     <a class="text-dark px-2" href="">
                         <i class="fab fa-facebook-f"></i>
@@ -38,7 +38,7 @@ const Header=()=>{
         </div>
         <div class="row align-items-center py-3 px-xl-5">
             <div class="col-lg-3 d-none d-lg-block">
-                <a href="" class="text-decoration-none">
+                <a href="" class="text-decoration-none text-dark">
                     <h1 class="m-0 display-5 font-weight-semi-bold "><span class="text-primary font-weight-bold border px-3 mr-1">E</span>Shopper</h1>
                 </a>
             </div>
@@ -55,13 +55,13 @@ const Header=()=>{
                 </form>
             </div>
             <div class="col-lg-3 col-6 text-right">
-                <a href="" class="btn border">
+                <a href="" class="btn border m-2">
                     <i class="fas fa-heart text-primary"></i>
-                    <span class="badge">0</span>
+                    <span class="badge text-dark">0</span>
                 </a>
                 <a href="" class="btn border">
                     <i class="fas fa-shopping-cart text-primary"></i>
-                    <span class="badge">0</span>
+                    <span class="badge text-dark">0</span>
                 </a>
             </div>
         </div>
