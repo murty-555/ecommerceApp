e44c7c46ec891532738747a417b5eb5bb61fa354
Naga Venkata Durga Mahesh <nsiriki@AD.MSYSTECHNOLOGIES.COM>
2022-12-22 09:22:27 +0530

    changes added

--- src/pages/Checkoutpage/Checkout.js
+++ src/pages/Checkoutpage/Checkout.js
@@ -5,7 +5,7 @@ export default function Checkout() {
   return (
     <div>
     <div class="container-fluid bg-secondary mb-5">
-        <div class="d-flex flex-column align-items-center justify-content-center" style={{"emin-height": "300px"}}>
+        <div class="d-flex flex-column align-items-center justify-content-center" style={{"min-height": "300px"}}>
             <h1 class="font-weight-semi-bold text-uppercase mb-3">Checkout</h1>
             <div class="d-inline-flex">
                 <p class="m-0"><a href="/">Home</a></p>
