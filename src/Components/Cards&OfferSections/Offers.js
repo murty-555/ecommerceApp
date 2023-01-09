e3f7ca4c68fb328548eebf54f129e5e9dbb69e04
Naga Venkata Durga Mahesh <nsiriki@AD.MSYSTECHNOLOGIES.COM>
2023-01-09 18:24:30 +0530

    padding changes

--- src/Components/Cards&OfferSections/Offers.js
+++ src/Components/Cards&OfferSections/Offers.js
@@ -9,7 +9,7 @@ const offers = () => {
                         <img className='girl-image' alt="girlpic" src={'images/offer-1.png'}></img>
                         <div className=' text-cont-right text-light'>
                             <h4 className='offer'>20% OFF THE ALL ORDER</h4>
-                            <h1 className='card-heading'>Spring Collection</h1>
+                            <h1 className='card-heading pb-2'>Spring Collection</h1>
                             <a className='button' href="@" >Shop Now</a>
                         </div>
                     </div>  
@@ -18,7 +18,7 @@ const offers = () => {
                     <div className='small-container P-L d-flex flex-row justify-content-between' style={{alignItems:'center'}}>
                         <div className='text-light'>
                             <h4 className='offer'>20% OFF THE ALL ORDER</h4>
-                            <h1 className='card-heading'>Winter Collection</h1>
+                            <h1 className='card-heading w-100 pb-2'>Winter Collection</h1>
                             <a className='button' href="@" >Shop Now</a>
                         </div>
                         <img className='girl-image' alt="girlpic" src={'images/offer-2.png'}></img>    
